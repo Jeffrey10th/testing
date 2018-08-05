077630f599c1b63152903e2ae1cd522d4a28aaac
Jeffrey10th <jeff12147212@gmail.com>
2018-08-05 10:09:48 +0800

    Update main.cpp - update program to test references and pointers.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,36 @@
 #include <iostream>
 
+/*  
+ * Program to test references, pointers and variables.
+ * Copyright (C) 2018 Jeff Tan Han Wei
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-/* Purpose of this program:
- * to test unsigned integers and signed integers and perform calculations between them.
- * This program is also (in terms of unsigned integers), related to deep-machine-level memory.
- * Use with caution.
-*/ 
+*/
 
 int main() {
 
-    unsigned int u = 10, u2 = 42;
-    int i = 10, i2 = 42;
+    //two definitions to start it all!
+    int i = 100, &r1 = i;
+    double d1 = r1;
+    d1 += 0.5;
 
-    //statement will work as expected since the resulting integer is not negative
-    std::cout << u2 - u << std::endl;
+    r1 = 101; //equivalent to i = 101
 
-    //resulting integer will be negative
-    std::cout << u - u2 << std::endl;
+    int &r2 = r1; //test if pointers can be assigned to another pointer
 
-    std::cout << i2 - i << std::endl;
-    std::cout << i - i2 << std::endl;
+    std::cout << i << " " << d1 << " " << r2 << std::endl; 
 
-    std::cout << i - u << std::endl;
-    std::cout << u - i << std::endl;
-    
     return 0;
 
 }
